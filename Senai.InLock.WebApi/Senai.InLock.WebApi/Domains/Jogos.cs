392c8f5f06bc01e25d3bc1001554dca456f92ce9
LucasPandolfi <lucasdatealive@gmail.com>
2020-03-02 17:32:45 -0300

    API no começo

---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Domains/Jogos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.InLock.WebApi.Domains
+{
+    public class Jogos
+    {
+        public int IdJogo { get; set; }
+
+        public string NomeJogo { get; set; }
+
+        public string DescricaoJogo { get; set; }
+
+        public DateTime DataLancamento { get; set; }
+
+        public double ValorJogo { get; set; }
+
+        public int IdEstudio { get; set; }
+    }
+}
